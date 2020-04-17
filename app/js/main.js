35bdce364f210048b6119133eb6301a88301e6c1
LitlleFinder <ischchenko.i@gmail.com>
2020-04-17 20:51:19 +0300

    problem range-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,31 @@
-$(function(){
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
+$(function () {
 
-    $('.product-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        $(".rate-star").rateYo({
+            rating: 5,
+            starWidth: "12px",
+            readOnly: true
+        });
+
+        $('.product-slider__inner').slick({
+            dots: true,
+            arrows: false,
+            slidesToShow: 4,
+            slidesToScroll: 4
+        });
+
+        var mixer = mixitup('.products__inner-box');
+
+       
     });
 
-    var mixer = mixitup('.products__inner-box');
-
-});
+     $(".js-range-slider").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 1000,
+            from: 200,
+            to: 500,
+            grid: true
+        });
+    
+  
